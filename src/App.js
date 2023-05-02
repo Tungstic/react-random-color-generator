eb5645826d5ec9c856de904106218fab2e1345ce
Natalia Budulchieva <nbudulchieva@gmail.com>
2023-05-02 17:05:20 +0200

    Implement basic features

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import './App.css';
+import randomColor from 'randomcolor';
+import { useState } from 'react';
 
 export default function App() {
-  return <div className="App"></div>;
+  const [color, setColor] = useState();
+  return (
+    <div className="App">
+      <div style={{ border: '2px solid black', backgroundColor: color }}>
+        <p>Generated Color: background color {color}</p>
+        <button onClick={() => setColor(randomColor())}>Generate</button>
+      </div>
+    </div>
+  );
 }
