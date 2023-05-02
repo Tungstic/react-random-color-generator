cedfb9f904a3d75f25d77192cb170771ad6b2eed
Natalia Budulchieva <nbudulchieva@gmail.com>
2023-05-02 19:54:35 +0200

    Add hue per user request

--- src/App.js
+++ src/App.js
@@ -4,11 +4,31 @@ import { useState } from 'react';
 
 export default function App() {
   const [color, setColor] = useState();
+  // const [hue, setHue] = useState('random');
+  // const [luminosity, setLuminosity] = useState('random');
+
   return (
     <div className="App">
       <div style={{ border: '2px solid black', backgroundColor: color }}>
         <p>Generated Color: {color}</p>
         <button onClick={() => setColor(randomColor())}>Generate</button>
+        <label>
+          Set hue:
+          <input
+            onChange={(event) =>
+              setColor(
+                randomColor({
+                  luminosity: 'bright',
+                  hue: event.currentTarget.value,
+                }),
+              )
+            }
+          />
+        </label>
+        <label>
+          Set luminosity:
+          <input />
+        </label>
       </div>
     </div>
   );
